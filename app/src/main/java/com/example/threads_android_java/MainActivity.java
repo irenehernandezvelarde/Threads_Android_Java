1686b7c88b15e03ce9b83c93da991adaf3e1232e
irenehernandezvelarde <ihernandezvelarde.cf@iesesteveterradas.cat>
2022-10-14 18:51:27 +0200

    //Add permissions on Manifest.

--- app/src/main/java/com/example/threads_android_java/MainActivity.java
+++ app/src/main/java/com/example/threads_android_java/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.threads_android_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +15,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -23,48 +27,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
         Button botoncito = (Button) findViewById(R.id.boton);
         botoncito.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                getDataFromUrl("https://api.myip.com");
+                executor.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        getDataFromUrl("https://api.myip.com");
+                    }
+                });
             }
         });
     }
-    String error = ""; // string field
-    private String getDataFromUrl(String demoIdUrl) {
-        String result = null;
-        int resCode;
-        InputStream in;
-        try {
-            URL url = new URL(demoIdUrl);
-            URLConnection urlConn = url.openConnection();
+            String error = ""; // string field
 
-            HttpsURLConnection httpsConn = (HttpsURLConnection) urlConn;
-            httpsConn.setAllowUserInteraction(false);
-            httpsConn.setInstanceFollowRedirects(true);
-            httpsConn.setRequestMethod("GET");
-            httpsConn.connect();
-            resCode = httpsConn.getResponseCode();
+            private String getDataFromUrl(String demoIdUrl) {
+                String result = null;
+                int resCode;
+                InputStream in;
+                try {
+                    URL url = new URL(demoIdUrl);
+                    URLConnection urlConn = url.openConnection();
 
-            if (resCode == HttpURLConnection.HTTP_OK) {
-                in = httpsConn.getInputStream();
+                    HttpsURLConnection httpsConn = (HttpsURLConnection) urlConn;
+                    httpsConn.setAllowUserInteraction(false);
+                    httpsConn.setInstanceFollowRedirects(true);
+                    httpsConn.setRequestMethod("GET");
+                    httpsConn.connect();
+                    resCode = httpsConn.getResponseCode();
 
-                BufferedReader reader = new BufferedReader(new InputStreamReader(
-                        in, "iso-8859-1"), 8);
-                StringBuilder sb = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    sb.append(line).append("\n");
+                    if (resCode == HttpURLConnection.HTTP_OK) {
+                        in = httpsConn.getInputStream();
+
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                                in, "iso-8859-1"), 8);
+                        StringBuilder sb = new StringBuilder();
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            sb.append(line).append("\n");
+                        }
+                        in.close();
+                        result = sb.toString();
+                    } else {
+                        error += resCode;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                in.close();
-                result = sb.toString();
-            } else {
-                error += resCode;
+                return result;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result;
     }
-
-}
