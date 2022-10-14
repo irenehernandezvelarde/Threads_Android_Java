c1098af4ba6f05a7c7115d43cf801aae457aa9c6
irenehernandezvelarde <ihernandezvelarde.cf@iesesteveterradas.cat>
2022-10-14 19:10:44 +0200

    //Make changes.

--- app/src/main/java/com/example/threads_android_java/MainActivity.java
+++ app/src/main/java/com/example/threads_android_java/MainActivity.java
@@ -2,17 +2,23 @@ package com.example.threads_android_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.concurrent.ExecutorService;
@@ -21,62 +27,67 @@ import java.util.concurrent.Executors;
 import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Handler handler = new Handler(Looper.getMainLooper());
+        Button button = findViewById(R.id.boton);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override public void onClick(View view) {
+                ExecutorService executor = Executors.newSingleThreadExecutor();
+                Handler handler = new Handler(Looper.getMainLooper());
 
-        Button botoncito = (Button) findViewById(R.id.boton);
-        botoncito.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
                 executor.execute(new Runnable() {
                     @Override
                     public void run() {
-                        getDataFromUrl("https://api.myip.com");
+                        Log.i("INFO:", getDataFromUrl("https://api.myip.com"));
+                        handler.post(new Runnable() {
+                            @Override public void run() {
+                                //UI Thread work here
+                            }
+                        });
                     }
                 });
             }
         });
     }
-            String error = ""; // string field
+    private String getDataFromUrl(String demoIdUrl) {
 
-            private String getDataFromUrl(String demoIdUrl) {
-                String result = null;
-                int resCode;
-                InputStream in;
-                try {
-                    URL url = new URL(demoIdUrl);
-                    URLConnection urlConn = url.openConnection();
+        String result = null;
+        int resCode;
+        InputStream in;
+        String error = "";
+        try {
+            URL url = new URL(demoIdUrl);
+            URLConnection urlConn = url.openConnection();
 
-                    HttpsURLConnection httpsConn = (HttpsURLConnection) urlConn;
-                    httpsConn.setAllowUserInteraction(false);
-                    httpsConn.setInstanceFollowRedirects(true);
-                    httpsConn.setRequestMethod("GET");
-                    httpsConn.connect();
-                    resCode = httpsConn.getResponseCode();
+            HttpsURLConnection httpsConn = (HttpsURLConnection) urlConn;
+            httpsConn.setAllowUserInteraction(false);
+            httpsConn.setInstanceFollowRedirects(true);
+            httpsConn.setRequestMethod("GET");
+            httpsConn.connect();
+            resCode = httpsConn.getResponseCode();
 
-                    if (resCode == HttpURLConnection.HTTP_OK) {
-                        in = httpsConn.getInputStream();
+            if (resCode == HttpURLConnection.HTTP_OK) {
+                in = httpsConn.getInputStream();
 
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(
-                                in, "iso-8859-1"), 8);
-                        StringBuilder sb = new StringBuilder();
-                        String line;
-                        while ((line = reader.readLine()) != null) {
-                            sb.append(line).append("\n");
-                        }
-                        in.close();
-                        result = sb.toString();
-                    } else {
-                        error += resCode;
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        in, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append("\n");
                 }
-                return result;
+                in.close();
+                result = sb.toString();
+            } else {
+                error += resCode;
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
+}
